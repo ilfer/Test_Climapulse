81098a1d2ddab4b727b8faca87619ee57c81794b
Ferdi Ileri <ferdiileri@Ferdis-MacBook-Air.local>
2025-10-07 22:58:04 +0200

    solved error vessel id by using vessel.id instead of id=1 which is deleted during testing

--- refrigerant/management/commands/withdraw.py
+++ refrigerant/management/commands/withdraw.py
@@ -8,28 +8,30 @@ class Command(BaseCommand):
     help = "Simulate condition when withdrawing refrigerant from a vessel."
 
     def handle(self, *args, **kwargs):
-        Vessel.objects.create(name="Test Vessel", content=50.0)
+        Vessel.objects.all().delete() # delete garbage
+        vessel = Vessel.objects.create(name="Test Vessel", content=50.0)
+        vessel_id = vessel.id # avoid hardcoded vessel id (error: refrigerant.models.Vessel.DoesNotExist: Vessel matching query does not exist. ), auto-increment in db (rows probably deleted by line 11)
         self.stdout.write("Simulating condition...")
-        self.run_simulation()
+        self.run_simulation(vessel_id)
 
-    def run_simulation(self):
+    def run_simulation(self, vessel_id):
         barrier = threading.Barrier(2)
 
         def user1():
             barrier.wait()
             with transaction.atomic():
-                vessel = Vessel.objects.select_for_update().get(id=1) # lock vessel row during runtime, prevent concurrent changes
+                vessel = Vessel.objects.select_for_update().get(id=vessel_id) # lock vessel row during runtime, prevent concurrent changes
                 vessel.content = max(vessel.content - 10.0, 0) # avoid negative value
                 vessel.save()
-        print("Thread 1 passed successfully!") # check if thread 1 ends successfully
+            print("Thread 1 passed successfully!") # check if thread 1 ends successfully
 
         def user2():
             barrier.wait()
             with transaction.atomic():
-                vessel = Vessel.objects.select_for_update().get(id=1) # lock vessel row during runtime, prevent concurrent changes
+                vessel = Vessel.objects.select_for_update().get(id=vessel_id) # lock vessel row during runtime, prevent concurrent changes
                 vessel.content = max(vessel.content - 10.0, 0) # avoid negative value
                 vessel.save()
-        print("Thread 2 passed successfully!") # check if thread 2 ends successfully
+            print("Thread 2 passed successfully!") # check if thread 2 ends successfully
 
         t1 = threading.Thread(target=user1)
         t2 = threading.Thread(target=user2)
@@ -38,5 +40,5 @@ class Command(BaseCommand):
         t1.join()
         t2.join()
 
-        vessel = Vessel.objects.get(id=1)
+        vessel = Vessel.objects.get(id=vessel_id)
         self.stdout.write(f"Remaining content: {vessel.content} kg")
